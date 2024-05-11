a7e24731d69d9802d9897abdddfdd33d16048a9b
Sergey Primakovich <prima.psa@gmail.com>
2024-05-11 22:39:41 +0300

    feat: add redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/prx/:path*',
+        destination: 'https://api.themoviedb.org/3/:path*',
+        headers: {"Authorization": `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMThlZjVjOTFjNDkzNDA5NGY2ZTk3YzUzNDEwYjQ1MyIsInN1YiI6IjY2M2Y5NmVjMTMyNzIxZjUxODIxMGJjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MHRIcWru0tXRfowkGqX1dJnfJoTCMAnKn3WDWY5ilYQ` }
+      },
+    ];
+  }
 };
 
 export default nextConfig;
