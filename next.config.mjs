c35961cfff807fec8d62ffaf04854b76cbf7c145
Sergey Primakovich <prima.psa@gmail.com>
2024-05-11 22:58:02 +0300

    feat: add header

--- next.config.mjs
+++ next.config.mjs
@@ -6,10 +6,22 @@ const nextConfig = {
       {
         source: '/prx/:path*',
         destination: 'https://api.themoviedb.org/3/:path*',
-        headers: {"Authorization": `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMThlZjVjOTFjNDkzNDA5NGY2ZTk3YzUzNDEwYjQ1MyIsInN1YiI6IjY2M2Y5NmVjMTMyNzIxZjUxODIxMGJjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MHRIcWru0tXRfowkGqX1dJnfJoTCMAnKn3WDWY5ilYQ` }
       },
     ];
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/prx/:path*',
+        headers: [
+          {
+            key: 'Authorization',
+            value: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMThlZjVjOTFjNDkzNDA5NGY2ZTk3YzUzNDEwYjQ1MyIsInN1YiI6IjY2M2Y5NmVjMTMyNzIxZjUxODIxMGJjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MHRIcWru0tXRfowkGqX1dJnfJoTCMAnKn3WDWY5ilYQ',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
